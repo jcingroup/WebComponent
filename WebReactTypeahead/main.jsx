e61a6cb0c4c2a36f6ee1ac7107812d0d4a6e0d46
jerryissky <jerryissky@gmail.com>
2015-11-26 11:28:00 +0800

    init project

---
+++ WebReactTypeahead/main.jsx
@@ -0,0 +1,63 @@
+﻿var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloMessage = React.createClass({
+
+    getInitialState: function () {
+
+        this.tid = 0;
+
+        return {
+            inputValue: null,
+            keyword: null,
+            visibility: 'hidden',
+            tid: null
+        };
+    },
+    onChange: function (e) {
+        var v = e.target.value;
+        if (v.trim() != '') {
+
+            this.setState({
+                keyword: v,
+                visibility: 'visible'
+            });
+
+            var _this = this;
+            var f = function () { _this.getData(v) };
+            console.log('before tid', this.tid);
+            if (this.tid == 0) {
+                this.tid = setTimeout(f, 500);
+            }
+            console.log('after tid', this.tid);
+        } else {
+            this.setState({
+                keyword: v,
+                visibility: 'hidden'
+            });
+        }
+    },
+    onClickItem: function (v, e) {
+        this.setState({ keyword: v, visibility: 'hidden' });
+    },
+    getData: function (m) {
+        clearTimeout(this.tid);
+        this.tid = 0;
+    },
+    render: function () {
+        var st = { visibility: this.state.visibility };
+        return (
+        <div>
+            <input type="text" value={this.state.keyword} onChange={this.onChange} />
+            <ul style={st}>
+                <li><a href="#" onClick={this.onClickItem.bind(this,'A')}>A Item</a></li>
+                <li><a href="#" onClick={this.onClickItem.bind(this,'B')}>B Item</a></li>
+                <li><a href="#" onClick={this.onClickItem.bind(this,'C')}>C Item</a></li>
+            </ul>
+        </div>
+        );
+    }
+});
+
+var mountNode = document.getElementById("context");
+ReactDOM.render(<HelloMessage />, mountNode);
---
+++ WebReactTypeahead/webpack.config.js
@@ -0,0 +1,27 @@
+﻿var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, 'main.jsx')
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+          { test: /\.jsx$/, loader: 'jsx-loader' },
+          { test: /\.css$/, loader: "style-loader!css-loader" },
+          { test: /\.less$/, loader: "style!css!less" }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', 'jsx', '.json']
+    },
+    plugins: [
+      //new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+      //new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /zh-tw/),
+      //new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
+    ]
+};
